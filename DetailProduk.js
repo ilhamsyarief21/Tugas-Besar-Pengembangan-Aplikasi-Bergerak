391cda980e5a4ed34e336d1df3395a1a2396d0ba
Ilham Syarief <115606564+ilhamsyarief21@users.noreply.github.com>
2023-11-12 13:17:58 +0700

    update

--- DetailProduk.js
+++ DetailProduk.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, handlePress } from 'react-native';
+import { View, Text, TouchableOpacity, handlePress,Image } from 'react-native';
 import * as Font from 'expo-font';
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons'; // Import FontAwesome from react-native-vector-icons
 
 const DetailProduk = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -41,13 +42,54 @@ const DetailProduk = () => {
   const handlePress = () => {
     navigation.navigate('Checkout'); // 'Checkout' adalah nama stack/halaman yang ingin Anda arahkan
   };
+  const ilham = () => {
+    navigation.navigate('Home'); // 'Checkout' adalah nama stack/halaman yang ingin Anda arahkan
+  }
+  const galleryImages = [
+    { source: require('./assets/image/3.jpeg'), width: 75, height: 75 },
+    { source: require('./assets/image/4.jpeg'), width: 75, height: 75 },
+    { source: require('./assets/image/5.jpeg'), width: 75, height: 75 },
+    { source: require('./assets/image/6.jpeg'), width: 75, height: 75 },
+  ];
 
   return (
     <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 50 }}>
-      <View style={{ width: 358.103, height: 324.965, borderRadius: 21.379, backgroundColor: 'lightblue', justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontFamily: 'raleway-medium' }}>React Native Untuk Pemula</Text>
-        <Text style={{ fontFamily: 'raleway-medium' }}>Course Untuk Pemula</Text>
-      </View>
+    <View style={{ width: 358.103, height: 324.965, borderRadius: 21.379, backgroundColor: 'lightblue', justifyContent: 'center', alignItems: 'center' }}>
+    
+    <Image
+        source={require('./assets/image/1.jpeg')}
+        style={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 21.379,
+          backgroundColor: 'black',
+          opacity: 0.7,
+        }}
+        resizeMode="cover"
+      />
+      <FontAwesome name="bed" size={15} color="white" style={{marginTop: -15, top: -30, left: -138}}/>
+      
+      <FontAwesome name="bath" size={15} color="white" style={{marginTop: -15, top: -32, left: -20}}/>
+
+      <Text style={{ fontFamily: 'raleway-bold', position: 'absolute', top: '60%', color: 'white', top: 170, left: 30, marginTop: 20  }}>React Native Untuk Pemula</Text>
+      
+      <Text style={{ fontFamily: 'raleway-medium', position: 'absolute', top: '63%', color: '#D7D7D7', left: 30 , marginTop: 20}}>Course Untuk Pemula</Text>
+    </View>
+    <TouchableOpacity onPress={ilham}>
+      <FontAwesome
+        name="angle-left"
+        size={25}
+        color="white"
+        style={{ marginTop: -25, top: -280, left: -150 }}
+      />
+      
+    </TouchableOpacity>
+    <FontAwesome
+        name="bookmark"
+        size={22}
+        color="white"
+        style={{ marginTop: -23, top: -280, left: 140 }}
+      />
       <Text style={{ 
         marginTop: 20, 
         marginLeft: 20, 
@@ -71,21 +113,45 @@ const DetailProduk = () => {
       <TouchableOpacity onPress={toggleShowMore} style={{ marginTop: 10, marginLeft: 20, ...buttonPosition }}>
         <Text style={{ color: '#49aee7', fontSize: 12 }}>{showMore ? 'Show Less' : 'Show More'}</Text>
       </TouchableOpacity>
-      <View style={{ width: 45, height: 45, borderRadius: 30, backgroundColor: '#0A8ED9', marginTop: -10, marginLeft: 20, left: -165 }} />
+      <Image
+        source={require('./assets/image/terbaru.png')} // Ganti dengan path gambar Anda
+        style={{
+          width: 45,
+          height: 45,
+          borderRadius: 30,
+          marginTop: -10,
+          marginLeft: 20,
+          left: -165,
+        }} />
+        <Text style={{ fontFamily: 'raleway-regular', position: 'absolute', top: '50%', color: 'white', top: 190, left: 80, marginTop: 137  }}>6 Bedroom</Text>
+        <Text style={{ fontFamily: 'raleway-regular', position: 'absolute', top: '50%', color: 'white', top: 189, left: 192, marginTop: 137  }}>4 Bathroom</Text>
+        
       <View style={{ marginTop: 20, paddingHorizontal: 20, left: -70, top: -65 }}>
         <Text style={{ fontSize: 17, fontFamily: 'raleway-medium' }}>Ilham Syarief</Text>
         <Text style={{ fontSize: 12, fontFamily: 'raleway-regular', color: '#858585' }}>Main Tutor</Text>
       </View>
       <View style={{ width: 29.31, height: 29.31, backgroundColor: '#82c4e9', marginTop: -90, top: -8, left: 100, borderRadius: 5 }} />
+      <FontAwesome name="phone" size={15} color="white" style={{ marginTop: -16, left: 100, top: -14}} />
       <View style={{ width: 29.31, height: 29.31, backgroundColor: '#82c4e9', marginTop: -90, top: 53, left: 140, borderRadius: 5 }} />
+      <FontAwesome name="comment" size={15} color="white" style={{marginTop: -10, top: 40, left: 140}}/>
 
       {/* Tulisan "Gallery" di bawahnya */}
       <Text style={{ fontSize: 17, fontFamily: 'raleway-medium', marginTop: 75, left: -145 }}>Gallery</Text>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20, marginTop: 10 }}>
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
+        {galleryImages.map((image, index) => (
+          <Image
+            key={index}
+            source={image.source}
+            style={{
+              width: image.width,
+              height: image.height,
+              borderRadius: 10,
+              marginHorizontal: 5,
+              marginLeft: 10,
+              left: -6,
+            }}
+          />
+        ))}
       </View>
       <Text style={{ fontSize: 12, color: '#858585', marginLeft: -320, top: 20, fontFamily: 'raleway-medium'  }}>Price</Text>
       <Text style={{ fontSize: 17, color: 'black', marginLeft: -190, top: 20, fontFamily: 'raleway-medium'  }}>Rp. 250.000 / paket</Text>
